b8a7da67892bdf36e8fab65fdfb5fecfd167173a
VitorAlexandre17 <126284475+VitorAlexandre17@users.noreply.github.com>
2023-08-08 11:05:20 -0300

    Add files via upload

---
+++ TarefaWeb/routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/cool', function(req, res, next) {
+  res.render('index', { title: 'you are so cool' });
+});
+
+router.get('/cool/beach', function(req, res, next) {
+  res.send('adoro praia' );
+});
+
+router.get('/ifc', function(req, res, next) {
+  res.render('index', { title: 'bem vindo ao IFC' });
+});
+
+module.exports = router;
